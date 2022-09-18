987da76b942fd17f23447e04b65b4d16a88770d1
scriptSQD <scriipt.fun@gmail.com>
2022-09-18 20:00:20 +0300

    * Update Renderer.hpp

--- RendererBase/Renderer.hpp
+++ RendererBase/Renderer.hpp
@@ -17,7 +17,7 @@ namespace Alius {
         virtual void BeginFrame(/* all the possible parameters: cameras, scene metadata, etc. */) = 0;
 
         virtual void
-        Draw(uint32_t vertexCount, uint32_t firstVertex, uint32_t instanceCount = 1, uint32_t firstInstance = 0) = 0;
+        Draw(uint32_t vertexCount, uint32_t firstVertex, uint32_t instanceCount, uint32_t firstInstance) = 0;
         // All the other overloads for different draw metadata
         // ...
 
@@ -25,13 +25,14 @@ namespace Alius {
 
     };
 
-    using RendererModuleCreator = std::function<std::unique_ptr<Renderer>()>;
+    using RendererModuleCreator = std::function<std::unique_ptr<Renderer>(size_t, size_t, const char*)>;
 
     /**
      * Append your module to this map for it to be recognizable by Alius at runtime.
      * @tparam key Your module identifier.
      * @tparam value Function that returns a unique_ptr of your module. (return type
-     * should be unique_ptr<Renderer>)
+     * should be unique_ptr<Renderer>). Is of type std::function<std::unique_ptr<Renderer>(size_t, size_t, const char*)>.
+     * Should accept size_t width, size_t height and const char* title for underlying window creation.
      */
     std::unordered_map <std::string, RendererModuleCreator> s_RendererModules{};
 
