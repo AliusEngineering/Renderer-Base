327e9e569fa08135e7a449a756b35b97904c7353
scriptSQD <scriipt.fun@gmail.com>
2022-09-26 21:04:32 +0300

    * Add PCH

---
+++ RendererBase/RendererBasePch.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Window.hpp"
+
+#include "RendererObjects.hpp"
+
+#include "cstdint"
+
+#include "functional"
+#include "string"
+#include "unordered_map"
+
+#include "memory"
